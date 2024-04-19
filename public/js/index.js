7176b7a3adb2ae6578d2699c1984a06c499b6a0b
Richard Copra <rickcopra@gmail.com>
2024-04-19 10:30:07 +0200

    added pseudo code for API interaction to follow project brief in index.js

---
+++ public/js/index.js
@@ -0,0 +1,8 @@
+// Event listener for search button
+// When the button is clicked, validate the input and then fetch data from the API
+document.getElementById('postInput').addEventListener('click', function() {
+  // Get the value from the input field
+  // If valid, create API request with input
+  // Use fetch to get data from the API, limit results to 10 restaurants
+  // Return following restaurant data: name, cuisines, rating - as a number, and address
+});
